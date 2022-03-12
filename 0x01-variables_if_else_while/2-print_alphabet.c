a6f950d3a8302715304a4bb7b9e6b891579bd80b
Curio <Curiodhorlahpo@gmail.com>
2022-03-12 07:05:49 -0800

    variables

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,13 +5,13 @@
  * Return: Always 0 (success)
  */
 int main(void)
-{		
+{
 char alp[26] = "abcdefghijklmnopqrstuvwxyz";
 int i;
 for (i = 0; i < 26; i++)
-	{
+{
 putchar(alp[i]);
-	}
+}
 putchar('\n');
 return (0);
 }
